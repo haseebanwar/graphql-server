8e1cd09b6b571934aca307772097c1763172bb00
Haseeb Anwar <haseebanwar99@gmail.com>
2021-09-23 16:00:36 +0500

    Initial setup

--- data/index.js
+++ data/index.js
@@ -1,34 +1,18 @@
-// const authors = [
-//   { id: 1, name: 'Haseeb Anwar', email: 'test@test.com', location: 'Karachi' },
-//   { id: 2, name: 'John Doe', email: 'johndoe@test.com', location: 'England' },
-//   { id: 3, name: 'Haider', email: 'haider@test.com', location: 'Lahore' },
-//   { id: 4, name: 'Usman Ahmed', email: 'usman@test.com', location: 'Karachi' },
-//   { id: 5, name: 'Cary Wong', email: 'cary@test.com', location: 'Canada' },
-//   { id: 6, name: 'Kyle', email: 'kyle@test.com', location: 'US' },
-// ];
-
-// const books = [
-//   { id: 1, name: 'Haseeb Anwar', email: 'test@test.com', location: 'Karachi' },
-//   { id: 2, name: 'John Doe', email: 'johndoe@test.com', location: 'England' },
-//   { id: 3, name: 'Haider', email: 'haider@test.com', location: 'Lahore' },
-//   { id: 4, name: 'Usman Ahmed', email: 'usman@test.com', location: 'Karachi' },
-// ];
-
 const authors = [
-  { id: 1, name: "J. K. Rowling" },
-  { id: 2, name: "J. R. R. Tolkien" },
-  { id: 3, name: "Brent Weeks" },
+  { id: 1, name: 'J. K. Rowling' },
+  { id: 2, name: 'Winston Groom' },
+  { id: 3, name: 'Brent Weeks' },
 ];
 
 const books = [
-  { id: 1, name: "Harry Potter and the Chamber of Secrets", authorId: 1 },
-  { id: 2, name: "Harry Potter and the Prisoner of Azkaban", authorId: 1 },
-  { id: 3, name: "Harry Potter and the Goblet of Fire", authorId: 1 },
-  { id: 4, name: "The Fellowship of the Ring", authorId: 2 },
-  { id: 5, name: "The Two Towers", authorId: 2 },
-  { id: 6, name: "The Return of the King", authorId: 2 },
-  { id: 7, name: "The Way of Shadows", authorId: 3 },
-  { id: 8, name: "Beyond the Shadows", authorId: 3 },
+  { id: 1, title: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
+  { id: 2, title: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
+  { id: 3, title: 'Harry Potter and the Goblet of Fire', authorId: 1 },
+  { id: 4, title: 'Shrouds of glory', authorId: 2 },
+  { id: 5, title: 'Better Times Than These', authorId: 2 },
+  { id: 6, title: 'Forrest Gump', authorId: 2 },
+  { id: 7, title: 'The Way of Shadows', authorId: 3 },
+  { id: 8, title: 'Beyond the Shadows', authorId: 3 },
 ];
 
 exports.authors = authors;
--- server.js
+++ server.js
@@ -1,33 +1,18 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
-const { loadSchemaSync } = require('@graphql-tools/load');
-const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader');
-const graphqlResolver = require('./resolvers');
 
-const app = express();
+const server = express();
 const PORT = process.env.PORT || 5000;
 
 const schema = makeExecutableSchema({
-  typeDefs: loadSchemaSync('schemas/**/*.graphql', {
-    loaders: [new GraphQLFileLoader()],
-  }),
-  resolvers: graphqlResolver,
+  typeDefs: `type Query {
+    _empty: String
+  }`,
+  resolvers: {},
 });
 
-app.use((req, res, next) => {
-  const token = req.header('Authorization');
-
-  // verify token
-
-  // bind user to req
-
-  // add property to req object
-  req.isAuthenticated = token;
-  next();
-});
-
-app.use(
+server.use(
   '/graphql',
   graphqlHTTP({
     schema,
@@ -35,4 +20,4 @@ app.use(
   })
 );
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+server.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
